edcadb766da4a32b50acc11c08d1aeb628d9ca27
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-21 14:10:23 -0500

    commit tree bdf2ffd7d8540a25116d05ef507783b3df213dfd

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+
+require 'pry'
+
+class School
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student,grade)
+        roster[grade] ||= []
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted_roster = roster.sort.to_h
+        sorted_roster.sort_by do |grade,students|
+            students.sort!
+        end
+        sorted_roster
+    end
+end
